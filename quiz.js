ef90ec87668b74120dce12a71644c03e0a5e4fc4
lvv5040 <66978068+lvv5040@users.noreply.github.com>
2020-07-19 19:32:08 -0600

    added questions

--- quiz.js
+++ quiz.js
@@ -10,3 +10,43 @@ const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
+
+// create our questions
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        imgSrc : "img/html.png",
+        choiceA : "Hypertext Markup Language",
+        choiceB : "Hard To Manage Lines",
+        choiceC : "Hack To Manage Language",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Coding Source Sites",
+        choiceB : "Cascading Style Sheets",
+        choiceC : "Correct Style Sheet",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Junior Sheet",
+        choiceB : "Just Style",
+        choiceC : "Javascript",
+        correct : "C"
+    }, {
+        question : "What does API stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Application Potential Included",
+        choiceB : "Acting Programing Interwebs",
+        choiceC : "Application Programming Interface",
+        correct : "C"
+    },{
+        question : "What does DOM stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Document Object Model",
+        choiceB : "Database Observation Mechanism",
+        choiceC : "Data Office Management",
+        correct : "A"  
+    }
+];
